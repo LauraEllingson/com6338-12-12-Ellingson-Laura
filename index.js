88acffc6a57483b6e2ef0fbf5dc1c0fbd4ecddd2
LauraEllingson <lellingson@ufl.edu>
2024-04-24 17:33:31 -0400

    solved code

--- index.js
+++ index.js
@@ -27,15 +27,83 @@ class Word {
 
   // implement the guessLetter function:
   // guessLetter(letter) {}
+  guessLetter (guessedLetter){
+    //check to see if a letter has already been guessed
+    if (this.correctLetters.indexOf(guessedLetter)>=0 || this.incorrectLetters.indexOf(guessedLetter)>=0){
+      return
+    }
+     //Should update displayWord with correctly guessed letters
+     //Should NOT update displayWord with incorrectly guessed letters
+    const displayWordAsArray = this.displayWord.split('');
+    this.word.split('').forEach( (letter, index) => {
+      if(letter === guessedLetter) {
+        displayWordAsArray[index] = guessedLetter;
+      }
+    });
+    const updatedDisplayWord = displayWordAsArray.join('');
+    //When the users guessed the letter correctly update the change
+    if(this.displayWord !== updatedDisplayWord){
+      this.displayWord = updatedDisplayWord;
+      //Should add correct letter to correctLetters property, not incorrectLetters property
+      this.correctLetters.push(guessedLetter);
+      //Should NOT decrement remainingGuesses when correctly guessing a letter
+    } else{
+      //Should decrement remainingGuesses when incorrectly guessing a letter
+      this.remainingGuesses--;
+      //Should add incorrect letter to incorrectLetters property, not correctLetters property
+      this.incorrectLetters.push(guessedLetter);
+    }
+  }
 
   // implement the updateScreen function:
   // updateScreen() {}
+  updateScreen (){
+     //Should update #word-to-guess with displayWord
+    const wordToGuessEl = document.getElementById('word-to-guess');
+    wordToGuessEl.textContent = this.displayWord
+    //Should update #remaining-guesses with remainingGuesses
+    const remainingGuessesEl = document.getElementById('remaining-guesses');
+    remainingGuessesEl.textContent = this.remainingGuesses;
+    //Should update #incorrect-letters with incorrectLetters
+    const incorrectLettersEl = document.getElementById('incorrect-letters');
+    incorrectLettersEl.textContent = this.incorrectLetters.join(', ');
+  }
 
   // implement the isGameOver function:
   // isGameOver() {}
-
+  isGameOver(){
+     //Should return true if displayWord !== word and remainingGuesses <= 0
+    if (this.displayWord!== this.word && this.remainingGuesses <= 0) {
+      console.log ('Game Over')
+      return true;
+    } 
+  //Should return true if displayWord === word and remainingGuesses > 0
+  //Should return true if displayWord === word and remainingGuesses <= 0
+    if (this.displayWord=== this.word && this.remainingGuesses >= 0) {
+      console.log ('Game Over')
+      return true;
+    } 
+    //Should return false if displayWord !== word and remainingGuesses > 0
+    return false;
+  } 
+  
   // implement the getWinOrLoss function:
   // getWinOrLoss() {}
+  //--------------------------------------------//
+  getWinOrLoss(){
+    const isGameOver = this.isGameOver()
+     //Should return null if game is not over
+    if (isGameOver === false) {
+      return null
+    }
+
+  //Should return "win" if displayWord === word and remainingGuesses > 0
+    if (isGameOver === true && this.word === this.displayWord){
+      return "win"
+    }
+     //Should return "loss" if remainingGuesses <= 0 and displayWord !== word
+    return "loss";
+  }
 }
 
 function newGame() {
